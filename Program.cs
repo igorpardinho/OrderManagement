ed0d3306fac3d3b92d69f4cf8601f7396518c132
Igor Pardinho <pardinhoigor@gmail.com>
2026-04-16 15:00:23 -0300

    chore: corrige erro de sintaxe em ConnectionStrings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Mapster;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.UseCases.Product;
 using OrderManagement.Data;
 using OrderManagement.Domain.Interfaces;
 using OrderManagement.Infrastructure.Repository;
@@ -7,8 +9,13 @@ using OrderManagement.Infrastructure.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<CreateProductUseCase>();
+builder.Services.AddScoped<GetProductsUseCase>();
+builder.Services.AddScoped<GetProductByIdUseCase>();
+builder.Services.AddScoped<UpdateProductUseCase>();
+builder.Services.AddScoped<DeleteProductUseCase>();
 builder.Services.AddMapster();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
