ab84b344678b6be5db86eef3ee068deb6a80adb0
Igor Pardinho <pardinhoigor@gmail.com>
2026-04-16 12:44:08 -0300

    feat: camada de infraestrutura com a implementação de ProductRepository e UOW

---
+++ Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Data;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Repository;
+
+
+namespace OrderManagement.Infrastructure.Repository
+{
+    public class ProductRepository(AppDbContext context) : IBaseRepository<Product>
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<Product> Create(Product entity)
+        {
+            await _context.Products.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async void Delete(Product entity)
+        {
+            await _context.Products.Where(x => x.Id == entity.Id).ExecuteDeleteAsync();
+
+        }
+
+        public async Task<(IEnumerable<Product> Items, int Total)> GetAll(int pageNumber, int pageSize)
+        {
+            var query = _context.Products.AsNoTracking();
+            var total = await _context.Products.CountAsync();
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, total);
+        }
+
+        public async Task<Product?> GetById(Guid id)
+        {
+            var product = await _context.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+
+            return product;
+        }
+
+        public void Update(Product entity)
+        {
+            _context.Products.Update(entity);
+        }
+    }
+}
---
+++ Infrastructure/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿
+
+using OrderManagement.Data;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Interfaces;
+using OrderManagement.Domain.Repository;
+using OrderManagement.Infrastructure.Repository;
+
+namespace OrderManagement.Infrastructure
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context = context;
+        private IBaseRepository<Product>? _productRepository;
+
+        public IBaseRepository<Product> Products => _productRepository ??= new ProductRepository(_context);
+
+        public async Task<bool> CommitAsync() => await _context.SaveChangesAsync() > 0;
+
+
+        public void Dispose() => _context.Dispose();
+
+    }
+}
