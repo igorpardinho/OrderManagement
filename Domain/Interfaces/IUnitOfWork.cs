31bba4dc5038daffec93590962bdeada8d781ff6
Igor Pardinho <pardinhoigor@gmail.com>
2026-04-16 12:42:35 -0300

    feat: camada de dominio com a entidade Product a interface de UOW e o IBaseRepository

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Repository;
+
+namespace OrderManagement.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IBaseRepository<Product> Products { get; }
+        Task<bool> CommitAsync();
+
+
+    }
+}
---
+++ Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.Domain.Repository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<(IEnumerable<T> Items, int Total)> GetAll(int pageNumber, int pageSize);
+        Task<T?> GetById(Guid id);
+        Task<T> Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
